bcbcdbfa854dafef2c30f7d9e6aa970d1412da71
Marco <marco.cusenza.me@gmail.com>
2021-11-25 18:13:25 +0100

    bonus

--- js/main.js
+++ js/main.js
@@ -2,5 +2,7 @@ const app = new Vue({
     el: '#root',
     data: {
         messaggio: "Hello World",
+        imgUrl: "https://www.architetturaecosostenibile.it/images/stories/2016/promuoviamo-paesaggio-italiano.jpg",
+        imgAlt: "paesaggio",
     }
 });
